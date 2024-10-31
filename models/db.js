b35e257e060179866d7959185f09931f53cd6bdb
Katie Duryea <155897343+katiegd@users.noreply.github.com>
2024-10-31 10:34:22 -0400

    Forgot to add the render database URL to the connection string.

--- models/db.js
+++ models/db.js
@@ -2,7 +2,8 @@ const { Pool } = require("pg");
 require("dotenv").config();
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL_LOCAL,
+  connectionString:
+    process.env.DATABASE_URL_LOCAL || process.env.DATABASE_URL_RENDER,
 });
 
 module.exports = pool;
