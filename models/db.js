377dbc84209e325a8a9cde85edc3c903763c5581
Katie Duryea <155897343+katiegd@users.noreply.github.com>
2024-10-31 10:39:22 -0400

    Addded SSL requirement to db.js

--- models/db.js
+++ models/db.js
@@ -4,6 +4,9 @@ require("dotenv").config();
 const pool = new Pool({
   connectionString:
     process.env.DATABASE_URL_LOCAL || process.env.DATABASE_URL_RENDER,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 module.exports = pool;
